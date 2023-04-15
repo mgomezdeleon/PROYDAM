bc510e4007680f0f5becc62714841f22f029fbe1
Marta <mgomezdeleon@ceinmark.net>
2023-04-15 19:40:03 +0200

    Primer commit

---
+++ src/prueba/Número.java
@@ -0,0 +1,15 @@
+package prueba;
+
+public class Número {
+ public static void main(String[] args) {
+  //Codigo por www.DesarrollarAplicaciones.com
+ int numero=1;
+ int limite=100;
+ while(numero <= 100){
+  if(numero%2!=0){
+   System.out.println(numero);
+  }
+  numero++;
+ }
+}
+}
